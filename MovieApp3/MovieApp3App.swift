b0b9272218dd2b34584749d5d662b2cd8b404724
Clyde Lartey <lartey.d@etoncollege.org.uk>
2022-11-06 17:04:08 +0000

    Initial Commit

---
+++ MovieApp3/MovieApp3App.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp3App.swift
+//  MovieApp3
+//
+//  Created by Clyde Lartey on 06/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
